165750c41931309b2a5865314ecca7d891777884
DanielKibatullin <dkibatullin40@stuy.edu>
2024-04-14 17:36:51 -0400

    worked on it, fixed a couple of errors that we had. the form for the food project works.

--- food_project_vue/vite.config.js
+++ food_project_vue/vite.config.js
@@ -1,16 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+const backendPath = '../food_project'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  plugins: [vue()],
+  base: '/static/vite/',
+  server: {
+    watch: {
+      ignored: []
+    }
+  },
+  build: {
+    manifest: true,
+    emptyOutDir: true,
+    outDir: backendPath + '/core/static/vite/',
+    rollupOptions: {
+      input: {
+        vue_food_edit: './src/apps/food_edit/food_edit.js'
+      }
     }
   }
 })
