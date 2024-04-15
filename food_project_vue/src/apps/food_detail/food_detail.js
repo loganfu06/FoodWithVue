313399e2fdbdd283f242bfa2f045094a368b974c
DanielKibatullin <dkibatullin40@stuy.edu>
2024-04-15 14:27:51 -0400

    more commiting

---
+++ food_project_vue/src/apps/food_detail/food_detail.js
@@ -0,0 +1,4 @@
+import 'vite/modulepreload-polyfill';
+import { createApp } from 'vue';
+import App from './food_detail.vue'
+createApp(App).mount("#app")
---
+++ food_project_vue/src/apps/food_detail/food_detail.vue
@@ -0,0 +1,18 @@
+<template>
+    Hi, is this working?
+</template>
+<script>
+export default {
+name: 'App',
+components: {
+},
+data: function() {
+return {
+}
+},
+methods: {
+},
+computed: {
+}
+}
+</script>
