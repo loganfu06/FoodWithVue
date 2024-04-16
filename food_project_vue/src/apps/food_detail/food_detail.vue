b04a83515013cb6d10b596e5a0356e8b327cef2b
DanielKibatullin <dkibatullin40@stuy.edu>
2024-04-15 20:35:10 -0400

    work

--- food_project_vue/src/apps/food_detail/food_detail.vue
+++ food_project_vue/src/apps/food_detail/food_detail.vue
@@ -1,20 +1,24 @@
 <template>
-    
+  <div>
+    <a href="/food">Food list</a><br /><br />
+    <a href="#">Update Food</a><br />
+    <a href="#">Delete Food</a><br />
+    <h1>{{ food_name }}</h1>
+    <h1>{{ food_price }}</h1>
+  </div>
 </template>
 <script>
 export default {
-name: 'App',
-components: {
-},
-data: function() {
-return {
-    food_price: ext_price,
-    food_name: ext_name,
-}
-},
-methods: {
-},
-computed: {
-}
+  name: 'App',
+  components: {},
+  data: function () {
+    return {
+      food_price: ext_price,
+      food_name: ext_name,
+      food_id: ext_id
+    }
+  },
+  methods: {},
+  computed: {}
 }
 </script>
